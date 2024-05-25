3aeaa67bf9c8ccd99bb355488e6d282e98cbdada
imtiaj <imtiajrex@gmail.com>
2024-05-25 22:50:48 +0600

    updated exports

--- apps/nativetailui/index.ts
+++ apps/nativetailui/index.ts
@@ -2,6 +2,6 @@ export * from "./src/primitives";
 
 export * from "./src/templates";
 export * from "./src/components";
-export { default as tw } from "./src/tw";
+export * from "./src/tw";
 export * from "./src/layout";
 export * from "./src/stepper";
